b1ffc77669f619f9068b69f9620cd7672050d8a1
studywell2 <studywellmail1@gmail.com>
2025-09-10 06:13:45 -0700

    message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let  menu = document.querySelector('.navbar-burger');
+let menu = document.getElementById('burger');
 let bar = document.getElementById('mainNav');
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle("is-active");
-    bar.classList.toggle("is-active");
+menu.addEventListener('click', function() {
+  menu.classList.toggle("is-active");
+  bar.classList.toggle("is-active");
 });
